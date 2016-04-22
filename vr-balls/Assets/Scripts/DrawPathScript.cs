f287f68cf95550b86e41990a3cc0ad1339e713e2
Izabela Nedyalkova <izabela.nedyalkova@scalefocus.com>
2016-04-22 14:57:12 +0300

    import iTweenEditor and create demo curve

---
+++ vr-balls/Assets/Scripts/DrawPathScript.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class DrawPathScript : MonoBehaviour
+{
+    private iTweenPath path;
+    private LineRenderer lineRenderer;
+
+    void Start()
+    {
+        path = GetComponent<iTweenPath>();
+        lineRenderer = GetComponent<LineRenderer>();
+        
+        Vector3[] nodes = iTweenPath.GetPath(path.pathName);
+        Vector3[] vector3s = PathControlPointGenerator(nodes);
+
+        //Line Draw:
+        Vector3 prevPt = Interp(vector3s, 0);
+        lineRenderer.SetPosition(0, prevPt);
+        lineRenderer.SetWidth(0.1f, 0.1f);
+        lineRenderer.SetColors(Color.red, Color.red);
+
+        //Gizmos.color = color;
+        int smoothAmount = nodes.Length * 20;
+        lineRenderer.SetVertexCount(smoothAmount + 1);
+
+        Debug.Log(smoothAmount);
+        for (int i = 1; i <= smoothAmount; i++)
+        {
+            Debug.Log(i);
+            float pm = (float)i / smoothAmount;
+            Vector3 currPt = Interp(vector3s, pm);
+            //Gizmos.DrawLine(currPt, prevPt);
+            lineRenderer.SetPosition(i, currPt);
+            prevPt = currPt;
+        }
+    }
+
+    private Vector3[] PathControlPointGenerator(Vector3[] path)
+    {
+        Vector3[] suppliedPath;
+        Vector3[] vector3s;
+
+        //create and store path points:
+        suppliedPath = path;
+
+        //populate calculate path;
+        int offset = 2;
+        vector3s = new Vector3[suppliedPath.Length + offset];
+        Array.Copy(suppliedPath, 0, vector3s, 1, suppliedPath.Length);
+
+        //populate start and end control points:
+        //vector3s[0] = vector3s[1] - vector3s[2];
+        vector3s[0] = vector3s[1] + (vector3s[1] - vector3s[2]);
+        vector3s[vector3s.Length - 1] = vector3s[vector3s.Length - 2] + (vector3s[vector3s.Length - 2] - vector3s[vector3s.Length - 3]);
+
+        //is this a closed, continuous loop? yes? well then so let's make a continuous Catmull-Rom spline!
+        if (vector3s[1] == vector3s[vector3s.Length - 2])
+        {
+            Vector3[] tmpLoopSpline = new Vector3[vector3s.Length];
+            Array.Copy(vector3s, tmpLoopSpline, vector3s.Length);
+            tmpLoopSpline[0] = tmpLoopSpline[tmpLoopSpline.Length - 3];
+            tmpLoopSpline[tmpLoopSpline.Length - 1] = tmpLoopSpline[2];
+            vector3s = new Vector3[tmpLoopSpline.Length];
+            Array.Copy(tmpLoopSpline, vector3s, tmpLoopSpline.Length);
+        }
+
+        return (vector3s);
+    }
+
+    private static Vector3 Interp(Vector3[] pts, float t)
+    {
+        int numSections = pts.Length - 3;
+        int currPt = Mathf.Min(Mathf.FloorToInt(t * (float)numSections), numSections - 1);
+        float u = t * (float)numSections - (float)currPt;
+
+        Vector3 a = pts[currPt];
+        Vector3 b = pts[currPt + 1];
+        Vector3 c = pts[currPt + 2];
+        Vector3 d = pts[currPt + 3];
+
+        return .5f * (
+            (-a + 3f * b - 3f * c + d) * (u * u * u)
+            + (2f * a - 5f * b + 4f * c - d) * (u * u)
+            + (-a + c) * u
+            + 2f * b
+        );
+    }
+}
