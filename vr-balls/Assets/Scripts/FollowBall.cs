7e6c8082412b1e198d0c8ce82a346296fb26345b
Izabela Nedqlkova <izabela.nedyalkova@amexis.net>
2016-04-27 22:06:01 +0300

    head and follow balls

---
+++ vr-balls/Assets/Scripts/FollowBall.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowBall : MonoBehaviour
+{
+    // Params
+    float moveSpeed = 1; // Move speed
+    float rotationSpeed = 500; // Speed of turning    
+
+    // Find game objects
+    public GameObject ball;
+
+    public void SetBall(GameObject newBall)
+    {
+        ball = newBall;
+    }
+
+    void FixedUpdate()
+    {
+        if (ball != null)
+        {
+            float diameter = ball.transform.localScale.x;
+            // If the distance is over 5 units
+            if (Vector3.Distance(ball.transform.position, transform.position) > diameter)
+            {
+                Vector3 direction = ball.transform.position - transform.position;
+
+                // Rotate towards player
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
+
+                // Move forward at specified speed
+                transform.position += transform.forward * moveSpeed * Time.deltaTime;
+            }
+        }
+    }
+}
---
+++ vr-balls/Assets/Scripts/HeadBall.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadBall : MonoBehaviour
+{
+    // Params
+    float moveSpeed = 1; // Move speed
+    float rotationSpeed = 100; // Speed of turning    
+
+    // Find game objects
+    public Vector3[] points;
+    private int currentPointIndex = 0;
+    private Vector3 destinationPoint;
+
+    void FixedUpdate()
+    {
+        if (points != null)
+        {
+            // If the distance is over 5 units
+            if (Vector3.Distance(destinationPoint, transform.position) < 0.03f)
+            {
+                ChangeDestinationPoint();
+            }
+            else
+            {
+                Vector3 direction = destinationPoint - transform.position;
+                // Rotate towards player
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
+
+                // Move forward at specified speed
+                transform.position += transform.forward * moveSpeed * Time.deltaTime;
+            }
+        }
+    }
+
+    public void SetPoints(Vector3[] newPoints)
+    {
+        points = newPoints;
+        destinationPoint = points[0];
+    }
+
+    private void ChangeDestinationPoint()
+    {
+        currentPointIndex += 1;
+
+        if (currentPointIndex >= points.Length)
+        {
+            Debug.Log("Gameover");
+        }
+        else
+        {
+            destinationPoint = points[currentPointIndex];
+        }
+    }
+}
